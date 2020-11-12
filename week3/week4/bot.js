c4840e85199eabf19606ce87d9090402ed36c4b0
joshodey <63563707+joshodey@users.noreply.github.com>
2020-11-12 05:32:20 +0100

    Update bot.js
    //swal is more stylish to consider using this script
    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>

--- week3/week4/bot.js
+++ week3/week4/bot.js
@@ -13,16 +13,16 @@ const checkUser = () => {
 if (age.value >= 12 && faceMask.value ==="yes") {
   allowed++;
   document.querySelector('#allowed').innerHTML = "Allowed visitors:" + allowed
-  alert(`welcome to the mall:${userName.value}`);
+  swal (`welcome to the mall:${userName.value}`);
 
 } else if (age.value < 12 && faceMask.value ==="yes"){
   denied++;
   document.querySelector('#denied').innerHTML = "Denied visitors:" + denied
-  alert(`sorry you must be 12 years old to be allowed in:${userName.value}`);
+ swal (`sorry you must be 12 years old to be allowed in:${userName.value}`);
 } else {
   denied++;
   document.querySelector('#denied').innerHTML = "Denied visitors:" + denied
-  alert(`Ooops sorry you are not allowed in:${userName.value}`);
+  swal (`Ooops sorry you are not allowed in:${userName.value}`);
 }
 userName.value = "";
 age.value = "";
